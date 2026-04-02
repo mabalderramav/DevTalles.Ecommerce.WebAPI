0650716aa24f5ae10f36db62409a50dc7f3f506b
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2026-04-01 20:36:16 -0400

    add new migration about Category due to there perform some changes

--- Migrations/20260402003030_Migration_Category.Designer.cs
+++ Migrations/20260402003030_Migration_Category.Designer.cs
@@ -4,6 +4,7 @@ using DevTalles.Ecommerce.WebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevTalles.Ecommerce.WebAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260402003030_Migration_Category")]
+    partial class Migration_Category
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
