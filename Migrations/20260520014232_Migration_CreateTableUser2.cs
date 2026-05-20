2d658d3e72b42856260f907486b44bc05d491c0b
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2026-05-19 21:51:06 -0400

    Adds new migration about Migration_CreateTableUser2

---
+++ Migrations/20260520014232_Migration_CreateTableUser2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevTalles.Ecommerce.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_CreateTableUser2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
